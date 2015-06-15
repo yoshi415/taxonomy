a6a43d5658a0bd64c7c7009fa8a1e1e5676f063d
MKS-Student <mks.student@hackreactor.com>
2015-06-15 12:20:26 -0700

    "finished all taxonomy"

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+  Grub.call(this)
+  this.job = "keep on growing";
+  this.age = 5
+  this.color = "yellow"
 };
 
+var bee1 = new Bee(5, "yellow")
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
